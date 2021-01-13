1c640ab013e5845613df4f8dfbef2eb6d89e8003
DenisFZelaya <48973732+DenisFZelaya@users.noreply.github.com>
2021-01-13 15:47:04 -0600

    Initial commit

---
+++ ConsoleApp-HashSet-CSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp_HashSet_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new HashSet<string>();
+
+            names.Add("Denis");
+            names.Add("Federico");
+            names.Add("Bladi");
+            names.Add("Fanny");
+
+            names.Remove("Denis"); // test eliminar este item
+
+            foreach(var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
